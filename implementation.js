353e0be876a176412909ef764c73ffd00e1e5023
Jordan Harband <ljharb@gmail.com>
2024-03-18 12:48:58 -0700

    [Deps] update `call-bind`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var DeletePropertyOrThrow = require('es-abstract/2023/DeletePropertyOrThrow');
-var Get = require('es-abstract/2023/Get');
-var HasProperty = require('es-abstract/2023/HasProperty');
-var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
-var Set = require('es-abstract/2023/Set');
+var DeletePropertyOrThrow = require('es-abstract/2024/DeletePropertyOrThrow');
+var Get = require('es-abstract/2024/Get');
+var HasProperty = require('es-abstract/2024/HasProperty');
+var LengthOfArrayLike = require('es-abstract/2024/LengthOfArrayLike');
+var Set = require('es-abstract/2024/Set');
 var ToObject = require('es-object-atoms/ToObject');
-var ToString = require('es-abstract/2023/ToString');
+var ToString = require('es-abstract/2024/ToString');
 
 var forEach = require('es-abstract/helpers/forEach');
 var MAX_SAFE_INTEGER = require('es-abstract/helpers/maxSafeInteger');
